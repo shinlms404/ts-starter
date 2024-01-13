6ba948f5434b90c41c1f1809bf8f0a2af2243921
Charlie Chan <shinlms404@gmail.com>
2024-01-14 00:25:54 +0800

    chore: update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-	tabWidth: 2,
-	useTabs: true,
-	semi: false,
-	singleQuote: true,
-	trailingComma: 'none',
-	arrowParens: 'avoid',
-	plugins: ['prettier-plugin-packagejson', 'prettier-plugin-organize-imports']
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-organize-imports']
 }
 
 module.exports = config
